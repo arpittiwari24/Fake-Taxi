a5b246d3569a2bbfe1dd3015404f0513b5893198
arpittiwari24 <100207940+arpittiwari24@users.noreply.github.com>
2023-10-21 19:13:22 +0530

    added new filesss

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -8,48 +8,48 @@ export default function IndiPost() {
     
     const {data: session} = useSession()
 
-  const [latestData, setLatestData] = useState<Post[]>([])
+//   const [latestData, setLatestData] = useState<Post[]>([])
 
-  useEffect(() => {
-    async function fetchData() {
-    try {
-        const postsResponse = await axios.get(`http://localhost:5000/api/post/${fetchedPost._id}`);
-        const fetchedPosts: Post[] = postsResponse.data;
+//   useEffect(() => {
+//     async function fetchData() {
+//     try {
+//         const postsResponse = await axios.get(`http://localhost:5000/api/post/${fetchedPost._id}`);
+//         const fetchedPosts: Post[] = postsResponse.data;
 
-        // // Fetch comments for each post
-        const postsWithComments = await Promise.all(
-            fetchedPosts.map(async (fetchedPost) => {
-                const commentsResponse = await axios.get(`http://localhost:5000/api/post/comment/post/${fetchedPost._id}`);
-                const comments : Comment[] = commentsResponse.data;
-                return { ...fetchedPost, comments };
-            })
-        );
+//         // // Fetch comments for each post
+//         const postsWithComments = await Promise.all(
+//             fetchedPosts.map(async (fetchedPost) => {
+//                 const commentsResponse = await axios.get(`http://localhost:5000/api/post/comment/post/${fetchedPost._id}`);
+//                 const comments : Comment[] = commentsResponse.data;
+//                 return { ...fetchedPost, comments };
+//             })
+//         );
 
-        setLatestData(postsWithComments);
-    } catch (error) {
-        console.error(error);
-    }
-      }
-      fetchData();
-    },[]);
+//         setLatestData(postsWithComments);
+//     } catch (error) {
+//         console.error(error);
+//     }
+//       }
+//       fetchData();
+//     },[]);
 
-  interface Post {
-    _id: string;
-    name: string;
-    title: string;
-    content: string;
-    comments: Comment[];
-  }
+//   interface Post {
+//     _id: string;
+//     name: string;
+//     title: string;
+//     content: string;
+//     comments: Comment[];
+//   }
 
-interface Comment {
-    _id: string;
-    name: string;
-    content: string;
-}
+// interface Comment {
+//     _id: string;
+//     name: string;
+//     content: string;
+// }
 
 return (
     <div>
-        {latestData.map((post) => (
+        {/* {latestData.map((post) => (
             <div key={post._id}>
                 <h2>{post.name}</h2>
                 <h2>{post.title}</h2>
@@ -64,7 +64,9 @@ return (
                     ))}
                 </ul>
             </div>
-        ))}
+        ))} */}
+
+        <h1 className="text-4xl text-black">Post</h1>
     </div>
 );
 }
